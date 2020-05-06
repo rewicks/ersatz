228154ebcec87354f0d96abbe27f30f0b066c236
Rachel Wicks <rachel@wicks.me>
2020-05-06 12:16:05 -0400

    scorer changed

--- score.py
+++ score.py
@@ -1,11 +1,53 @@
 import argparse, numpy
 
 def stream(path):
-    with open(path, 'r') as f:
-        for line in f:
-            for c in line:
-                yield c
+    content = open(path).read()
+    while content != content.replace('  ', ' '):
+        content = content.replace('  ', ' ')
+    content = content.replace(u'\u00a0', ' ')
+    content = content.replace(' ', ' <mos> ')
+    content = content.replace('\n', ' <eos> ')
+    f = content.split()    
+    for index, word in enumerate(f):
+        if word == '<eos>' or word == '<mos>':
+            #print(f[index-10:index+1])
+            yield word, f[index-10:index+10]
 
+def score(split_path):
+    language = split_path.split('/')[1]
+    correct_path = '/home/hltcoe/rwicks/ersatz/data/raw/' + language + '/test/' + '/'.join(split_path.split('/')[2:])   
+
+    correct = stream(correct_path)
+    split = stream(split_path)
+
+    results = numpy.zeros((2,2))
+    type_one = open('type_one.txt', 'a+')
+    type_two = open('type_two.txt', 'a+')
+
+    EOF = False
+    while (not EOF):
+        try:
+            #print('correct')
+            c, debug_context = next(correct)
+            #print('split')
+            s, context = next(split)
+       
+            print(f"Corr: {' '.join(debug_context)}\nPred: {' '.join(context)}\n\n") 
+            if c == '<eos>' and s == '<eos>':
+                results[0][0] += 1
+            elif c == '<eos>' and s != '<eos>':
+                results[0][1] += 1
+                type_two.write('Corr: ' + ' '.join(debug_context) + '\nPred: ' + ' '.join(context) + '\n\n')
+            elif c != '<eos>' and s == '<eos>':
+                results[1][0] += 1
+                type_one.write('Corr: ' + ' '.join(debug_context) + '\nPred: ' + ' '.join(context) + '\n\n')
+            else:
+                results[1][1] += 1
+        except:
+            EOF = True
+    return results
+
+'''
 def score(correct_path, split_path):
     correct = stream(correct_path)
     split = stream(split_path)
@@ -14,59 +56,69 @@ def score(correct_path, split_path):
     type_one = open('type_one.txt', 'a+')
     type_two = open('type_two.txt', 'a+')
 
+    type_one.write(split_path + '\n')
+    type_two.write(split_path + '\n')
     context = ['' for x in range(50)]
-
+    debug_context = ['' for x in range(50)]
     EOF = False
     while (not EOF):
         try:
             c = next(correct)
-            if c != ' ':
+            debug_context.pop(0)
+            debug_context.append(c)
+            while c != ' ' and c != '\n':
+                c = next(correct)
+                debug_context.pop(0)
+                debug_context.append(c)
+            
+            s = next(split)            
+            context.pop(0)
+            context.append(s)
+            while s != ' ' and s != '\n':
                 s = next(split)
                 context.pop(0)
-                context.append(s)
-                while s == ' ':
-                    s = next(split)
-                    context.pop(0)
-                    context.append(s)
-                if c == '\n' and s == '\n':
-                    results[0][0] += 1
-                elif c == '\n' and s != '\n':
-                    c= next(correct)
-                    while c != s:
-                        c = next(correct)
-                    results[0][1] += 1
-                    type_two.write(''.join(context) + '\n\n')
-                elif c != '\n' and s == '\n':
-                    results[1][0] += 1
-                    type_one.write(''.join(context) + '\n')
-                    while s != c:
-                        s = next(split)
-                        context.pop(0)
-                        context.append(s)
-                else:
-                    results[1][1] += 1
+                context.append(s)            
+
+            if c == '\n' and s == '\n':
+                results[0][0] += 1
+            elif c == '\n' and s != '\n':
+                results[0][1] += 1
+                type_two.write(''.join(context) + '\n\n')
+            elif c != '\n' and s == '\n':
+                results[1][0] += 1
+                type_one.write(''.join(context) + '\n')
+            else:
+                results[1][1] += 1
         except:
             EOF = True
-
     return results
-
+'''
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('map_file')
+    #parser.add_argument('map_file')
+    parser.add_argument('split_files', nargs='*')
 
     args = parser.parse_args()
 
     results = numpy.zeros((2,2))
 
-    with open(args.map_file, 'r') as f:
-        for line in f:
-            line = line.split()
-            r = score(line[1], line[0])    
-            print(f'Results on {line[1]}')
-            print(r)
-            results += r
-    
+    scores = open('scores.txt', 'a+')
+
+    for fi in args.split_files:
+        r = score(fi)
+        print(fi)
+        print(r)
+        prec = (r[0][0]/(r[0][0]+r[1][0]))*100
+        recall = (r[0][0]/(r[0][0]+r[0][1]))*100
+        f1 = 2*(prec*recall)/(prec+recall)
+        print(f'{fi}\t{prec:.1f},{recall:.1f},{f1:.1f}')
+        #print(f'Recall : {recall}')
+        #print(f'Precision : {prec}')
+        #print(f'Accuracy : {((r[0][0]+r[1][1])/(r[0][0] + r[1][0] + r[1][1]))*100}')
+        results += r
+        #(f'{fi}\t{prec:.1f}\t{recall:.1f}\t{f1:.1f}') 
+        scores.write(f'{fi}\t{prec:.1f}\t{recall:.1f}\t{f1:.1f}\n')
     print("Overall Results:")
     print(results)
     acc = (results[0][0]+results[1][1])

d28fe797ebf5a7b7c4a2be1d06365e2627a9e7ab
Rachel Wicks <rw3yp@virginia.edu>
2021-01-11 22:08:31 -0500

    spm implementation and a bit of refactor

--- split.py
+++ split.py
@@ -11,12 +11,9 @@ from determiner import *
 
 logging.basicConfig(format="%(levelname)s : %(message)s", level=logging.INFO)
 
-Batch = namedtuple("Batch", "contexts labels indices")
 
-#global_det = MultilingualPunctuation()
-#global_det = Split()
-global_det = PunctuationSpace()
-
+# default args for loading models
+# should write something to merge default args with loaded args (overwrite when applicable)
 class DefaultArgs():
     def __init__(self):
         self.left_context_size=4
@@ -28,14 +25,12 @@ class DefaultArgs():
         self.linear_nlayers=0
         self.activation_type='tanh'
 
+
 def load_model(checkpoint_path):
     model_dict = torch.load(checkpoint_path, map_location=torch.device('cpu'))
-    model = ErsatzTransformer(model_dict['vocab'], model_dict['args'])
-    #model = ErsatzTransformer(model_dict['vocab'], DefaultArgs())
-    #model = ErsatzTransformer(model_dict['vocab'], model_dict['left_context_size'], model_dict['right_context_size'], embed_size=model_dict['embed_size'], nhead=model_dict['nhead'], t_dropout=model_dict.get('t_dropout', 0.1), e_dropout=model_dict.get('e_dropout', 0.5), num_layers=2)
+    model = ErsatzTransformer(model_dict['tokenizer'], model_dict['args'])
     model.load_state_dict(model_dict['weights'])
     model.eval()
-    logging.info(model)
     return model
 
 def detokenize(input_string):
@@ -51,148 +46,93 @@ class EvalModel():
                 self.model = self.model.module
             self.device = torch.device("cuda") 
             self.model = self.model.to(self.device)  
-            self.vocab = self.model.vocab
-            self.left_context_size = self.model.left_context_size
-            self.right_context_size = self.model.right_context_size
-            self.context_size = self.left_context_size + self.right_context_size 
-            #self.spm_path = spm_path
             if torch.cuda.device_count() > 1:
                 self.model = torch.nn.DataParallel(self.model)
         else:
             self.model = load_model(model_path)
-            #self.model = torch.load(model_path, map_location=torch.device('cpu'))
             if type(self.model) is torch.nn.DataParallel:
                 self.model = self.model.module
             self.device = torch.device('cpu')
             self.model.to(self.device)
-            self.vocab = self.model.vocab
-            #self.context_size = self.model.context_size
-            self.left_context_size = self.model.left_context_size
-            self.right_context_size = self.model.right_context_size
-            self.context_size = self.right_context_size + self.left_context_size
-            #self.spm_path = spm_path
 
-    def evaluate(self, file_path):
-        output = []
-        content = open(file_path, 'r').read().strip()
-        content = content.split()
-        for index, token in enumerate(content):
-            temp_index = index-1
-            left_context = []
-            right_context = []
-            if '\u2581' in token:
-                while (len(left_context) < self.left_context_size):
-                    if temp_index >= 0:
-                        left_context.append(content[temp_index])
-                    else:
-                        left_context.append('<PAD>')
-                    temp_index -= 1
-                left_context.reverse()
-                temp_index = index 
-                while (len(right_context) < self.right_context_size):
-                    if temp_index < len(content):
-                        right_context.append(content[temp_index])
-                    else:
-                        right_context.append('<PAD>')
-                    temp_index += 1
-                context, _ = self.vocab.context_to_tensor([(left_context, right_context, '<hole>')])
-                context = context.view(-1, self.context_size)
-                if self.model.module.predict_word(context) == '<eos>':
-                    output.append('\n')
-            output.append(token)
-        output = ''.join(output)
-        output = output.strip()
-        return output
+        self.tokenizer = self.model.tokenizer
+        self.left_context_size = self.model.left_context_size
+        self.right_context_size = self.model.right_context_size
+        self.context_size = self.right_context_size + self.left_context_size
 
     def batchify(self, file_path, batch_size, det):
-        for d in dataset.split_test_file(file_path, self.left_context_size, self.right_context_size):
+        for d in dataset.split_test_file(file_path, self.tokenizer, self.left_context_size, self.right_context_size):
             if len(d[0]) > 0:
                 left_contexts = d[0]
                 right_contexts = d[1]
                 lines = []
                 indices = []
                 index = 0
-                #import pdb; pdb.set_trace()
                 for left, right in zip(left_contexts, right_contexts):
                     if det(detokenize(' '.join(left)), detokenize(' '.join(right))):
                         lines.append((left, right, '<eos>'))
                         indices.append(index)
                     index += 1 
                 indices = torch.tensor(indices) 
-                data, labels = self.vocab.context_to_tensor(lines)
+                data, _ = self.tokenizer.context_to_tensor(lines)
          
                 nbatch = data.size(0) // batch_size
                 remainder = data.size(0) % batch_size 
                 
                 if remainder > 0:
                     remaining_data = data.narrow(0,nbatch*batch_size, remainder)
-                    remaining_labels = labels.narrow(0, nbatch*batch_size, remainder)
-                    remaining_indices = indices.narrow(0, nbatch*batch_size, remainder) 
+                    remaining_indices = indices.narrow(0, nbatch*batch_size, remainder)
                 
                 data = data.narrow(0, 0, nbatch*batch_size)
-                labels = labels.narrow(0, 0, nbatch * batch_size)
                 indices = indices.narrow(0, 0, nbatch * batch_size)
 
                 data = data.view(batch_size, -1).t().contiguous()
-                labels = labels.view(batch_size, -1).t().contiguous()
                 indices = indices.view(batch_size, -1).t().contiguous()
                 
                 if remainder > 0:
                     remaining_data = remaining_data.view(remainder, -1).t().contiguous()
-                    remaining_labels = remaining_labels.view(remainder, -1).t().contiguous()
                     remaining_indices = remaining_indices.view(remainder, -1).t().contiguous()
          
 
                 batches = []
                 data = data.view(-1, self.context_size, batch_size)
-                labels = labels.view(-1, 1, batch_size)
                 indices = indices.view(-1, 1, batch_size)
 
                 if remainder > 0:
                     remaining_data = remaining_data.view(-1, self.context_size, remainder)
-                    remaining_labels = remaining_labels.view(-1, 1, remainder)
-                    remaining_indicies = remaining_indices.view(-1, 1, remainder)
-                for context_batch, label_batch, index_batch in zip(data, labels, indices):
-                    batches.append(Batch(context_batch.t(), label_batch[0], index_batch[0]))
+                    remaining_indices = remaining_indices.view(-1, 1, remainder)
+                for context_batch, index_batch in zip(data, indices):
+                    batches.append((context_batch.t(), index_batch[0]))
                 if remainder > 0:
-                    batches.append(Batch(remaining_data[0].t(), remaining_labels[0][0], remaining_indices[0]))
+                    batches.append((remaining_data[0].t(), remaining_indices[0][0]))
                 yield batches
             else:
                 yield None
 
     def parallel_evaluation(self, input_path, batch_size, det=None):
-        start = time.time()
-        logging.info('starting batchify...')
         batches = self.batchify(input_path, batch_size, det)
-        logging.info(f'batching took {time.time()-start} seconds')
         eos = []
         content = open(input_path).read().split('\n')
         for i, doc in enumerate(batches):
             if doc is not None:
-                for batch in doc:
-                    data = batch.contexts.to(self.device)
-                    labels = batch.labels.to(self.device)
-                    indices = torch.tensor(batch.indices)
+                for contexts, indices in doc:
+                    data = contexts.to(self.device)
             
                     output = self.model.forward(data)
                    
                     pred = output.argmax(1)
-                    pred_ind = pred ^ labels
-                    pred_ind = torch.where(pred_ind == 0)[0]
+                    pred_ind = torch.where(pred == 0)[0]
                     pred_ind = [indices[i].item() for i in pred_ind]
                     eos.extend(pred_ind)
-                logging.info(f'finished making predictions...sorting...')
                 if len(eos) == 0:
-                    #print(content[i].strip())
                     yield content[i]
                 else:
                     eos = sorted(eos)
                     next_index = int(eos.pop(0))
-                    this_content = content[i].split()
+                    this_content = self.tokenizer.encode(content[i], out_type=str)
                     output = ''
                     counter = 0
                     for index, word in enumerate(this_content):
-                        #print(next_index,counter)
                         if counter == next_index:
                             output += '\n' + word
                             try:
@@ -202,6 +142,7 @@ class EvalModel():
                         else:
                             output += ' ' + word
                         counter += 1
+                    output = self.tokenizer.merge(output)
                     yield output
             else:
                 yield ''
@@ -213,20 +154,19 @@ if __name__ == '__main__':
     parser.add_argument('--input')
     parser.add_argument('--output')
     parser.add_argument('--batch_size', type=int)
-    #parser.add_argument('--spm_path', default='/home/hltcoe/rwicks/ersatz/models/en.wiki.10000.model')
+    parser.add_argument('--determiner_type', default='en', choices=['en', 'multilingual', 'all'])
 
     args = parser.parse_args()
+
+    if args.determiner_type == "en":
+        determiner = PunctuationSpace()
+    elif args.determiner_type == 'multilingual':
+        determiner = MultilingualPunctuation()
+    else:
+        determiner = Split()
+
     model = EvalModel(args.model_path)
-    model.model.eval()
-    start = time.time()
-    for output in model.parallel_evaluation(args.input, args.batch_size, det=global_det):
+    for output in model.parallel_evaluation(args.input, args.batch_size, det=determiner):
         output = output.split('\n')
         output = [o.strip() for o in output]
         print('\n'.join(output))
-    elapsed = time.time()-start
-    logging.info(f'{elapsed} seconds for: {args.input}')
-    '''
-    with open(args.output, 'w') as f:
-        for line in output:
-            f.write(line.strip() + '\n')
-    '''
---
+++ subword.py
@@ -0,0 +1,115 @@
+# *-* coding: utf-8 *-*
+import torch
+
+class Vocabulary():
+
+    def __init__(self):
+        self.itos = ['<unk>', '<eos>', '<mos>', '<pad>']
+        self.stoi = {'<unk>': 0, '<eos>': 1, '<mos>': 2, '<pad>': 3}
+
+    def __len__(self):
+        return len(self.itos)
+
+    def build_vocab(self, file_path):
+        with open(file_path) as i:
+            for line in i:
+                word = line.split()[0].strip()
+                self.add_word(word)
+
+    def add_word(self, word):
+        if word not in self.stoi:
+            self.stoi[word] = len(self.itos)
+            self.itos.append(word)
+
+    def embed_word(self, word):
+        return self.stoi.get(word, 2)
+
+    def get_word(self, embedding):
+        return self.itos[embedding]
+
+    def detokenize(self, input_string):
+        input_string = input_string.replace(' ', '')
+        input_string = input_string.replace('\u2581', ' ')
+        return input_string
+
+    def encode(self, input_string, out_type=int):
+        if out_type is int:
+            arr = []
+            input_string = input_string.split()
+            for s in input_string:
+                arr.append(self.embed_word(s))
+            return arr
+        else:
+            return input_string.split()
+
+    def decode(self, input_array):
+        output = []
+        for i in input_array:
+            output.append(self.get_word(i))
+        return ' '.join(output)
+
+    def tensor_to_string(self, tensors):
+        if len(tensors.shape) > 1:
+            output = []
+            for tens in tensors:
+                output.append(self.decode(tens.tolist()))
+            return output
+        else:
+            return self.decode(tensors.tolist())
+
+    def context_to_tensor(self, contexts):
+        con_arr = []
+        lab_arr = []
+        for left, right, label in contexts:
+            tens = []
+            for l in left.split():
+                tens.append(self.embed_word(l))
+            for r in right.split():
+                tens.append(self.embed_word(r))
+            con_arr.append(tens)
+            if label == "<eos>":
+                lab_arr.append(0)
+            else:
+                lab_arr.append(1)
+        return torch.tensor(con_arr), torch.tensor(lab_arr)
+
+class SentencePiece(Vocabulary):
+    """
+    Implements SentencePiece.
+    https://github.com/google/sentencepiece/blob/master/python/README.md
+    """
+    def __init__(self, model_path, vocab_path = None, sample: bool = True, alpha: float = 0.5):
+        import sentencepiece as spm
+        self.model = spm.SentencePieceProcessor()
+        self.model_path = model_path
+        if model_path is not None:
+            self.model.Load(model_path)
+        if vocab_path:
+            self.vocab_path = vocab_path
+            self.model.LoadVocabulary(vocab_path)
+        self.alpha = alpha
+        self.sample = sample
+
+    def __len__(self):
+        return self.model.get_piece_size()
+
+    def embed_word(self, word):
+        return self.model[word]
+
+    def encode(self, sentence, out_type=int) -> str:
+        return self.model.encode(sentence, out_type=out_type)
+        if out_type is int:
+            if self.sample:
+                return ' '.join(self.model.SampleEncodeAsPieces(sentence, nbest_size=1, alpha=self.alpha))
+            else:
+                return ' '.join(self.model.EncodeAsPieces(sentence))
+
+    def decode(self, ids):
+        return self.model.decode(ids)
+
+    def merge(self, sentence: str) -> str:
+        return sentence.replace(' ', '').replace('▁', ' ')
+
+
+def get_tokenizer(model_path, sample = False):
+    return SentencePiece(model_path, sample=sample)
